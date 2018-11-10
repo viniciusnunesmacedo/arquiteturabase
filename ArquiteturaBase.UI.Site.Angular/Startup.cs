f16354a016f6af0ad9593c7b13e65bb17406db99
Vinicius Nunes Macedo <vnmacedo@gmail.com>
2018-11-10 18:18:03 -0200

    Inclusão do SPA em Angular

--- ArquiteturaBase.UI.Site.Angular/Startup.cs
+++ ArquiteturaBase.UI.Site.Angular/Startup.cs
@@ -1,17 +1,12 @@
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
-using ArquiteturaBase.Infraestrutura.Transversal.InversaoControle;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
-using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.HttpsPolicy;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.SpaServices.AngularCli;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 
-namespace ArquiteturaBase.UI.Site.Mvc
+namespace ArquiteturaBase.UI.Site.Angular
 {
     public class Startup
     {
@@ -23,20 +18,15 @@ namespace ArquiteturaBase.UI.Site.Mvc
         public IConfiguration Configuration { get; }
 
         // This method gets called by the runtime. Use this method to add services to the container.
-        public IServiceProvider ConfigureServices(IServiceCollection services)
+        public void ConfigureServices(IServiceCollection services)
         {
-            services.Configure<CookiePolicyOptions>(options =>
-            {
-                // This lambda determines whether user consent for non-essential cookies is needed for a given request.
-                options.CheckConsentNeeded = context => true;
-                options.MinimumSameSitePolicy = SameSiteMode.None;
-            });
-
-
             services.AddMvc().SetCompatibilityVersion(CompatibilityVersion.Version_2_1);
 
-            // .NET Native DI Abstraction
-            return RegistrarServicos(services);
+            // In production, the Angular files will be served from this directory
+            services.AddSpaStaticFiles(configuration =>
+            {
+                configuration.RootPath = "ClientApp/dist";
+            });
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
@@ -54,15 +44,27 @@ namespace ArquiteturaBase.UI.Site.Mvc
 
             app.UseHttpsRedirection();
             app.UseStaticFiles();
-            app.UseCookiePolicy();
+            app.UseSpaStaticFiles();
 
-            app.UseMvc();
-        }
+            app.UseMvc(routes =>
+            {
+                routes.MapRoute(
+                    name: "default",
+                    template: "{controller}/{action=Index}/{id?}");
+            });
 
-        private static IServiceProvider RegistrarServicos(IServiceCollection servicos)
-        {
-            // Adding dependencies from another layers (isolated from Presentation)
-            return AutoFacInjetor.RegistrarServicos(servicos);
+            app.UseSpa(spa =>
+            {
+                // To learn more about options for serving an Angular SPA from ASP.NET Core,
+                // see https://go.microsoft.com/fwlink/?linkid=864501
+
+                spa.Options.SourcePath = "ClientApp";
+
+                if (env.IsDevelopment())
+                {
+                    spa.UseAngularCliServer(npmScript: "start");
+                }
+            });
         }
     }
 }
