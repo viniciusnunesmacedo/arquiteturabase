399163752f93caa1f0910414e977fbb084842b8c
Vinicius Nunes Macedo <vnmacedo@gmail.com>
2018-11-10 10:54:21 -0200

    versão inicial

---
+++ ArquiteturaBase.Aplicacao/Class1.cs
@@ -0,0 +1,8 @@
+﻿using System;
+
+namespace ArquiteturaBase.Aplicacao
+{
+    public class Class1
+    {
+    }
+}
---
+++ ArquiteturaBase.Dominio/Class1.cs
@@ -0,0 +1,8 @@
+﻿using System;
+
+namespace ArquiteturaBase.Dominio
+{
+    public class Class1
+    {
+    }
+}
